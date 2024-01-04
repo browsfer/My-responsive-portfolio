6ea7a5c76081f54b3959e9226be99437ff43a1a5
browsfer <126813633+browsfer@users.noreply.github.com>
2024-01-04 16:38:28 +0100

    fixed desktop scaffold with missing projects list

--- lib/responsive/scaffolds/desktop_scaffold.dart
+++ lib/responsive/scaffolds/desktop_scaffold.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_project/utils/my_drawer.dart';
-import 'package:responsive_project/utils/projects_list/my_list_tile.dart';
+import 'package:responsive_project/utils/projects_list/my_projects_list.dart';
 
 class MyDesktopScaffold extends StatelessWidget {
   MyDesktopScaffold({super.key});
@@ -9,42 +9,10 @@ class MyDesktopScaffold extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: Row(
+      body: const Row(
         children: [
           //Body
-          Expanded(
-            flex: 4,
-            child: ListView(
-              padding: const EdgeInsets.symmetric(
-                vertical: 50,
-                horizontal: 10,
-              ),
-              children: [
-                MyListTile(
-                  leadingIcon: Icons.attach_money_rounded,
-                  title: 'BANKITO',
-                  projectDescription:
-                      'Bankito - mobile banking app made from the scratch using FirebaseAuth and Firestore, also SQLite to save data locally.',
-                  projectUrl: 'https://github.com/browsfer/Bankito',
-                ),
-                MyListTile(
-                  projectUrl:
-                      'https://github.com/browsfer/My-responsive-portfolio',
-                  title: 'RESPONSIVE PORTFOLIO',
-                  leadingIcon: Icons.person_rounded,
-                  projectDescription:
-                      'My portfolio app - a responsive showcase of my skills and expertise, crafted with modern Material3 themes and engineered with efficiency using Provider for state management, Shared Preferences for data persistence and Geolocation for personalized content tailoring.',
-                ),
-                MyListTile(
-                  projectUrl: 'https://github.com/browsfer/Chatify',
-                  title: 'CHATIFY',
-                  leadingIcon: Icons.chat_rounded,
-                  projectDescription:
-                      'Chatify - simple mobile chat app made from the scratch.',
-                )
-              ],
-            ),
-          ),
+          MyProjectsList(),
 
           //Right side open drawer
           Expanded(
