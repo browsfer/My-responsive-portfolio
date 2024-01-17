faa4e235998e0e8a73df4a9d0719220aec055540
browsfer <126813633+browsfer@users.noreply.github.com>
2024-01-17 21:03:44 +0100

    Added two small buttons to Play/pause music

--- lib/utils/my_floating_button.dart
+++ lib/utils/my_floating_button.dart
@@ -17,18 +17,18 @@ class _MyFloatingActionButtonState extends State<MyFloatingActionButton>
   //Global key to initialize tooltip
   final GlobalKey _toolTipKey = GlobalKey();
 
-  //Music button animation
-  bool _isPlaying = false;
-
   //Music variables
   final player = AudioPlayer();
   String trackId = '6rWblGW0pBcB3uygxBuWZV';
+  //Music button animation
+  bool _showPlayStopButton = false;
+  bool _isPlaying = false;
 
   @override
   void initState() {
     //Activate tooltip by ensureTooltipVisible
     Future.delayed(
-      const Duration(seconds: 3),
+      Duration.zero,
       () {
         final dynamic tooltip = _toolTipKey.currentState;
         tooltip?.ensureTooltipVisible();
@@ -51,42 +51,79 @@ class _MyFloatingActionButtonState extends State<MyFloatingActionButton>
           var audioUrl = manifest.audioOnly.first.url;
           await player.setSourceUrl(audioUrl.toString());
         }
-        print(player.state);
       },
     );
-
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Tooltip(
-      key: _toolTipKey,
-      message: 'Let\'s dance?',
-      preferBelow: false,
-      showDuration: const Duration(seconds: 4),
-      textAlign: TextAlign.center,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12),
-          color: Theme.of(context).colorScheme.onBackground),
-      triggerMode: TooltipTriggerMode.manual,
-      child: FloatingActionButton(
-        //Temporary. Later it will be replaced by turn on/off music button
-        onPressed: () {
-          setState(() {
-            _isPlaying = !_isPlaying;
-            if (!_isPlaying) {
-              player.pause();
-            } else {
-              player.resume();
-            }
-          });
-        },
-        child: Lottie.asset(
-          'assets/floating_music_button_animation.json',
-          animate: _isPlaying ? true : false,
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        if (_showPlayStopButton)
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              //
+              //Two buttons to control music
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    player.resume();
+                    _showPlayStopButton = false;
+                    _isPlaying = true;
+                  });
+                },
+                icon: const Icon(Icons.play_circle_outline_rounded),
+              ),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    player.pause();
+                    _showPlayStopButton = false;
+                    _isPlaying = false;
+                  });
+                },
+                icon: const Icon(Icons.pause),
+              ),
+            ],
+          )
+        else
+          const SizedBox(height: 0),
+        Align(
+          alignment: Alignment.bottomRight,
+          child: Tooltip(
+            key: _toolTipKey,
+            message: 'Let\'s dance?',
+            preferBelow: false,
+            showDuration: const Duration(seconds: 4),
+            textAlign: TextAlign.center,
+
+            //TOOLTIP STYLE
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: Theme.of(context).colorScheme.onBackground),
+            triggerMode: TooltipTriggerMode.manual,
+            child: FloatingActionButton(
+              //
+              //TURN ON/OFF MUSIC
+              onPressed: () {
+                setState(() {
+                  _showPlayStopButton = !_showPlayStopButton;
+                });
+              },
+
+              //MUSIC BUTTON ANIMATION
+              child: Lottie.asset(
+                'assets/floating_music_button_animation.json',
+                animate: _isPlaying ? true : false,
+              ),
+            ),
+          ),
         ),
-      ),
+      ],
     );
   }
 }
